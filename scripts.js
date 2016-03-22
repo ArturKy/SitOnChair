a1fa119f87000993ffb1b868d233ab7a8ee910c5
ArturKy <tenfajny@gmail.com>
2016-03-22 14:42:21 +0100

    cleaning repo trashes

---
+++ scripts.js
@@ -0,0 +1,31 @@
+
+// znikanie i pojawianie się białego bloku
+
+$(document).ready(function(){
+
+  $(".top_graphics").mouseover(function(event){
+    event.stopPropagation();
+    $(this).find(".white_bg").fadeOut();
+  });
+  $(".top_graphics").mouseout(function(){
+    $(this).find(".white_bg").fadeIn();
+});
+});
+
+// Slider w Jumbotronie
+var $slides, index, max;
+$(document).ready(function(){
+  $slides = $(".slide")
+  max = $slides.length;
+  index = 0;
+
+var switchSlide = function(slideNumber) {
+  $slides.eq(index).toggleClass("hide");
+  index++;
+  if (index >= max) {index = 0};
+  $slides.eq(index).toggleClass("hide");
+};
+
+setInterval(switchSlide, 2000);
+
+});
